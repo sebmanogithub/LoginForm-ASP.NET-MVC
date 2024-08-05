4510b3ba7198251320ec560890bc23ba98102d03
sebastien.manolios <sebastien.manolios@weda.fr>
2024-08-05 16:38:44 +0200

    Lookup user accounts from SQL database

--- Services/Data/SecurityDAO.cs
+++ Services/Data/SecurityDAO.cs
@@ -1,6 +1,7 @@
 ﻿using LoginMVCWebApp.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +9,43 @@ namespace LoginMVCWebApp.Services.Data
 {
     public class SecurityDAO
     {
+        string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Test;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
         internal bool FindByUser(UserModel user)
         {
-            return (user.UserName == "Admin" && user.Password == "Secret");
+            bool success = false;
+
+            string queryString = $"SELECT * from dbo.Users WHERE" +
+                $" username=@Username AND password = @Password";
+
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand(queryString, sqlConnection);
+                cmd.Parameters.Add("@Username", System.Data.SqlDbType.VarChar, 50).Value = user.UserName;
+                cmd.Parameters.Add("@Password", System.Data.SqlDbType.VarChar, 50).Value = user.Password;
+
+                try
+                {
+                    sqlConnection.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.HasRows)
+                    {
+                        success = true;
+                    }
+                    else
+                    {
+                        success = false;
+                    }
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            return success;
+
         }
     }
 }
