75fabf8e5b829508f3563adc5a0b1bd4c5f28192
sebmanogithub <sebastiengithub@gmail.com>
2024-08-05 07:45:42 +0200

    Initial Commit

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using LoginMVCWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginMVCWebApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View("Login");
+        }
+
+        public string Login(UserModel userModel)
+        {
+            return "Results: UserName = " + userModel.UserName + " PW = " + userModel.Password;
+        }
+
+
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LoginMVCWebApp.MvcApplication" Language="C#" %>
